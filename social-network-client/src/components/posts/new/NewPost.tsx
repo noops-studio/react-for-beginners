c455b4b8cda262236143e02687e6d82c6535cc04
shaharsol <shahar@gnift.com>
2024-11-06 19:57:48 +0200

    useForm

--- social-network-client/src/components/posts/new/NewPost.tsx
+++ social-network-client/src/components/posts/new/NewPost.tsx
@@ -1,9 +1,28 @@
+import { useForm } from 'react-hook-form'
 import './NewPost.css'
+import PostDraft from '../../../models/post/PostDraft';
+import profileService from '../../../services/profileService';
 
 function NewPost(): JSX.Element {
+
+    const { register, handleSubmit } = useForm<PostDraft>();
+
+    async function submit(draft: PostDraft) {
+        console.log(draft)
+        try {
+            // const newPost = await profileService.create(draft)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
     return (
         <div className='NewPost'>
-            new post
+            <form onSubmit={handleSubmit(submit)}>
+                <input placeholder="enter a title..." { ...register('title')}/>
+                <textarea placeholder='enter a body...' {...register('body')}></textarea>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
