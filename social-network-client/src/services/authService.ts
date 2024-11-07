0e24b14321ff2f5452be69640619de352a36b394
shaharsol <shahar@gnift.com>
2024-11-07 18:40:17 +0200

    auth

---
+++ social-network-client/src/services/authService.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import Login from "../models/auth/Login";
+
+class AuthService {
+    async login(login: Login): Promise<string> {
+        const response = await axios.post<{jwt: string}>(`${process.env.REACT_APP_REST_SERVER}/auth/login`)
+        const { jwt } = response.data
+        return jwt
+    }
+}
+
+const authService = new AuthService();
+export default authService
