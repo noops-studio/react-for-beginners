ebec326706b407b39c09ea53f9cb9be5811e6dd4
shaharsol <shahar@gnift.com>
2024-11-13 14:16:35 +0200

    socket

--- social-network-client/src/components/socket/dispatcher/SocketDispatcher.tsx
+++ social-network-client/src/components/socket/dispatcher/SocketDispatcher.tsx
@@ -1,35 +1,51 @@
-import { PropsWithChildren, useEffect } from "react";
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import Post from "../../../models/post/Post";
 import useUserId from "../../../hooks/useUserId";
 import { useAppDispatch } from "../../../redux/hooks";
 import { addOnTop } from "../../../redux/profileSlice";
+import { v4 } from "uuid";
+
+export const SocketContext = createContext({
+    xClientId: ''
+})
 
 function SocketDispatcher(props: PropsWithChildren): JSX.Element {
 
     const userId = useUserId()
     const dispatch = useAppDispatch()
 
+    const [ xClientId, setXClientId ] = useState<string>(v4())
+    const value = { xClientId }
+
     useEffect(() => {
         const socket = io(process.env.REACT_APP_IO_SERVER)
 
-        socket.on('new-post', (post: Post) => {
+        // socket.onAny((eventName, payload) => {
+        //     console.log(eventName, payload)
+        // })
+        socket.on('new-post', (data: {from: string, post: Post}) => {
+            const { from, post } = data
             /*
             only if the new post is by MY user AND it is was not created by this app instance
             only then i want to dispatch the message
             */            
-            if (post.userId === userId) {
+            if (post.userId === userId && from !== xClientId) {
                 dispatch(addOnTop(post))
             }
         })
+
+        return () => {
+            socket.disconnect()
+        }
 
     }, [])
 
 
     return (
-        <>
+        <SocketContext.Provider value={value}>
             {props.children}
-        </>
+        </SocketContext.Provider>
     )
 }
 
--- social-network-client/src/hooks/useService.ts
+++ social-network-client/src/hooks/useService.ts
@@ -2,14 +2,17 @@ import axios, { AxiosInstance } from "axios";
 import { useContext } from "react";
 import { AuthContext } from "../components/auth/auth/Auth";
 import AuthAwareService from "../services/authAware/AuthAwareService";
+import { SocketContext } from "../components/socket/dispatcher/SocketDispatcher";
 
 export default function useService<T extends AuthAwareService>(Service: { new(axiosInstance: AxiosInstance): T}): T {
 
     const { jwt } = useContext(AuthContext)
+    const { xClientId } = useContext(SocketContext)
 
     const axiosInstance = axios.create({})
     axiosInstance.interceptors.request.use(config => {
         config.headers['Authorization'] = `Bearer ${jwt}`
+        config.headers['x-client-id'] = xClientId
         return config
     })
 
