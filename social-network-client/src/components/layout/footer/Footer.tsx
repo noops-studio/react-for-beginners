6a54433543c3aeb3debbd0ae492babefe9cef239
shaharsol <shahar@gnift.com>
2024-11-14 13:06:39 +0200

    docker

--- social-network-client/src/components/layout/footer/Footer.tsx
+++ social-network-client/src/components/layout/footer/Footer.tsx
@@ -5,6 +5,7 @@ function Footer(): JSX.Element {
     return (
         <div className='Footer'>
             <Stats />
+            <span>server is {process.env.REACT_APP_REST_SERVER}</span>
         </div>
     )
 }
