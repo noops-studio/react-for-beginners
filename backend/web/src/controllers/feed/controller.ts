b3505065da007e6647cad43c0eded27ec0d1fbc1
shaharsol <shahar@gnift.com>
2024-12-20 23:36:23 +0200

    getting extended posts with User in feed

--- backend/web/src/controllers/feed/controller.ts
+++ backend/web/src/controllers/feed/controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import Post from "../../models/Post";
 import sequelize from "../../db/sequelize";
+import User from "../../models/User";
 
 export async function getUserFeed(req: Request, res: Response, next: NextFunction) {
     const feed = await sequelize.query(`
@@ -12,7 +13,8 @@ export async function getUserFeed(req: Request, res: Response, next: NextFunctio
         order by posts.created_at desc
     `, {
         replacements: [req.userId],
-        model: Post
+        model: Post,
     })
+    await Promise.all(feed.map(post => post.reload({include:[User]})))
     res.json(feed)
 }
