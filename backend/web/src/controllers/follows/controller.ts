e26dd1b4d9341f4ab2b979851fec21f07a443d89
shaharsol <shahar@gnift.com>
2024-12-22 09:45:09 +0200

    follows

--- backend/web/src/controllers/follows/controller.ts
+++ backend/web/src/controllers/follows/controller.ts
@@ -1,32 +1,51 @@
 import { NextFunction, Request, Response } from "express";
 import Follow from "../../models/Follow";
 import User from "../../models/User";
-
-export async function getUserFollowing(req: Request, res: Response, next: NextFunction) {
-    const user = await User.findByPk(req.userId, {
-        include: [{
-            model: User,
-            as: 'followers'
-        },{
-            model: User,
-            as: 'following'
-        }]
-    })
-    res.json(user.following)
-}
+import sequelize from "../../db/sequelize";
 
 export async function getUserFollowers(req: Request, res: Response, next: NextFunction) {
-    // const followerId = '4b1193cc-7ba1-462c-99c5-2e3ea4ab6d14'
-    const user = await User.findByPk(req.userId, {
-        include: [{
-            model: User,
-            as: 'followers'
-        },{
-            model: User,
-            as: 'following'
-        }]
+    // const user = await User.findByPk(req.userId, {
+    //     include: [{
+    //         model: User,
+    //         as: 'followers'
+    //     },{
+    //         model: User,
+    //         as: 'following'
+    //     }]
+    // })
+    // res.json(user.following)
+    const followers = await sequelize.query(`
+        select * from users where id in (
+            select follower_id from follows where followee_id = ?
+        )
+    `, {
+        replacements: [req.userId],
+        model: User,
     })
-    res.json(user.followers)
+    res.json(followers)
+}
+
+export async function getUserFollowing(req: Request, res: Response, next: NextFunction) {
+    // const followerId = '4b1193cc-7ba1-462c-99c5-2e3ea4ab6d14'
+    // const user = await User.findByPk(req.userId, {
+    //     include: [{
+    //         model: User,
+    //         as: 'followers'
+    //     },{
+    //         model: User,
+    //         as: 'following'
+    //     }]
+    // })
+    // res.json(user.followers)
+    const followers = await sequelize.query(`
+        select * from users where id in (
+            select followee_id from follows where follower_id = ?
+        )
+    `, {
+        replacements: [req.userId],
+        model: User,
+    })
+    res.json(followers)
 }
 
 export async function follow(req: Request, res: Response, next: NextFunction) {
@@ -41,14 +60,14 @@ export async function follow(req: Request, res: Response, next: NextFunction) {
 
 export async function unfollow(req: Request, res: Response, next: NextFunction) {
     const userId = req.userId
-    const userToFollowId = req.params.id;
-    const follow = await Follow.findOne({
+    const userToUnfollowId = req.params.id;
+    console.log(userToUnfollowId)
+    await Follow.destroy({
         where: {
             followerId: userId,
-            followeeId: userToFollowId
+            followeeId: userToUnfollowId
         }
     })
-    await follow.destroy()
     res.json({
         success: true
     })
