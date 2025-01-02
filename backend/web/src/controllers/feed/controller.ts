28688c6a63b6ee039ee2b403e125b0d7ab2856d8
shaharsol <shahar@gnift.com>
2025-01-03 00:35:37 +0200

    correct sql

--- backend/web/src/controllers/feed/controller.ts
+++ backend/web/src/controllers/feed/controller.ts
@@ -2,19 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import Post from "../../models/Post";
 import sequelize from "../../db/sequelize";
 import User from "../../models/User";
+import Comment from "../../models/Comment";
 
 export async function getUserFeed(req: Request, res: Response, next: NextFunction) {
     const feed = await sequelize.query(`
         select  posts.*
-        from    users
-        join    users as following on users.id = following.id
-        join    posts on posts.user_id = following.id
-        where   users.id = ?
-        order by posts.created_at desc
+        from    posts 
+        join    follows on  follows.followee_id = posts.user_id
+                        and follows.follower_id = ?
     `, {
         replacements: [req.userId],
         model: Post,
     })
-    await Promise.all(feed.map(post => post.reload({include:[User]})))
+    await Promise.all(feed.map(post => post.reload({include:[
+        User, {
+            model: Comment,
+            include: [User]
+        },
+    ]})))
     res.json(feed)
 }
